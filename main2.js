ddd45a3545df55f6b7ff836c2d6a21f67d3e6ae3
Mario <mc.fialho@hotmail.com>
2022-06-02 16:21:57 -0300

    update

---
+++ main2.js
@@ -0,0 +1,31 @@
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("div.slider");
+const btnLeft = document.getElementById("btn-left");
+const btnRight = document.getElementById("btn-right")
+
+let idx = 0
+
+btnLeft.addEventListener("click", e => {
+    e.preventDefault();
+    idx--;
+
+    if(idx < 0) {
+        idx = 3;
+
+        return imgs.style.transform = `translateX(${-idx * 650}px)`;
+    }    
+
+    imgs.style.transform = `translateX(${-idx * 650}px)`;
+})
+
+btnRight.addEventListener("click", e => {
+    e.preventDefault();
+    idx++;
+
+    if(idx > img.length -1) {       
+        idx = 0;
+    }    
+
+    imgs.style.transform = `translateX(${-idx* 650}px)`;
+})
+
